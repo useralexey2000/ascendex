13e06f5c3bd7fd01e5e262e60206b9d8a50fee86
alex <useralexey2000@gmail.com>
2023-04-03 17:15:15 +0200

    cleaning code

--- main.go
+++ main.go
@@ -40,46 +40,4 @@ func main() {
 	for bbo := range ch {
 		fmt.Println("Bbo: ", bbo)
 	}
-
-	// url := "wss://ascendex.com/0/api/pro/v1/stream"
-
-	// // u := url.URL{Scheme: "ws", Host: addr, Path: "/echo"}
-	// c, _, err := websocket.DefaultDialer.Dial(url, nil)
-	// if err != nil {
-	// 	log.Fatal("dial:", err)
-	// }
-	// defer c.Close()
-
-	// // insure connection
-	// mt, message, err := c.ReadMessage()
-	// if err != nil {
-	// 	log.Println("read:", err)
-	// 	return
-	// }
-	// log.Printf("recv: %s, mt: %v", message, mt)
-
-	// data := `{ "op": "sub", "id": "abc123", "ch":"bbo:BTC/USDT" }`
-
-	// // subscribe to chan
-	// err = c.WriteMessage(1, []byte(data))
-	// if err != nil {
-	// 	log.Println("write:", err)
-	// 	return
-	// }
-
-	// done := make(chan struct{})
-	// go func() {
-	// 	defer close(done)
-	// 	for {
-	// 		mt, message, err := c.ReadMessage()
-	// 		if err != nil {
-	// 			log.Println("read:", err)
-	// 			return
-	// 		}
-	// 		log.Printf("recv: %s, mt: %v", message, mt)
-	// 	}
-	// }()
-
-	// time.Sleep(50 * time.Second)
-
 }
